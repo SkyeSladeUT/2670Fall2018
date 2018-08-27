913a1652226f4e379facc93b2a3b6204a3f66a2f
Unknown <skyesladeut@outlook.com>
2018-08-27 14:30:45 -0600

    Event Systems

---
+++ 2670Fall/Assets/Scripts/EventScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventScript : MonoBehaviour
+{
+
+    public UnityEvent MyEvent;
+    public UnityEvent MyEvent2;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        MyEvent.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        MyEvent2.Invoke();
+    }
+}
