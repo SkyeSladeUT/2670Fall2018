ce6f8a74363371d8caf309fc9414832ae8d889c5
Unknown <skyesladeut@outlook.com>
2018-09-19 18:56:33 -0600

    Assignment0

---
+++ Assignment0/Assets/Scripts/AIS/AIAgent.cs
@@ -0,0 +1,38 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.AI;
+[RequireComponent(typeof(NavMeshAgent))]
+public class AIAgent : MonoBehaviour
+{
+	public FloatData speed;
+	private NavMeshAgent agent;
+	public Transform Player;
+	public Transform Postpoint;
+	private Transform finalDestination;
+	
+	private void Start()
+	{
+		finalDestination = Postpoint;
+		agent.speed = speed.Value;
+		agent = GetComponent<NavMeshAgent>();
+
+	}
+
+	private void Update()
+	{
+		//agent.destination = finalDestination.position;
+	}
+
+	private void OnTriggerEnter(Collider obj)
+	{
+		if (obj.transform == Player)
+		{
+			finalDestination = Player;
+		}
+	}
+
+	private void OnTriggerExit(Collider obj)
+	{
+		finalDestination = Postpoint;
+	}
+}
---
+++ Assignment0/Assets/Scripts/Collector.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Collector : MonoBehaviour
+{
+	public PLayer player;
+	private int ShieldLife;
+	public Text ShieldLifeText;
+	public GameObject ShieldOutline;
+	public Text ScoreText;
+
+	private void Start()
+	{
+		ShieldOutline.SetActive(false);
+		player.Score = 0;
+		ScoreText.text = "Score: " + player.Score.ToString();
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		
+		switch (other.tag) 
+		{
+				case "Shield" :
+					Destroy(other.gameObject);
+					player.IsShieldActive = true;
+					ShieldOutline.SetActive(true);
+					ShieldLife = 10;
+					StartCoroutine("Shield");
+					break;
+				case "Coin":
+					Destroy(other.gameObject);
+					player.Score += 10;
+					ScoreText.text = "Score: " + player.Score.ToString();
+					break;
+				case "Enemy" :
+					if (!player.IsShieldActive)
+					{
+						player.Score -= 10;
+						ScoreText.text = "Score: " + player.Score.ToString();
+					}
+					break;
+				default:
+					break;
+		}
+	}
+
+	IEnumerator Shield()
+	{
+		while (ShieldLife > 0)
+		{
+			ShieldLifeText.text = ShieldLife.ToString();
+			ShieldLife--;
+			yield return new WaitForSeconds(1);
+			if (ShieldLife <= 0)
+			{
+				player.IsShieldActive = false;
+				ShieldOutline.SetActive(false);
+				ShieldLifeText.text = "";
+			}
+		}
+		
+	}
+}
