574789e6f95c3b4fd6988b0fbe523b5870c7c4a0
Unknown <skyesladeut@outlook.com>
2018-10-10 18:25:12 -0600

    Worked on game

---
+++ HyperCasualGame/Assets/Scripts/ObstacleScriptw/UpDownMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpDownMovement : MonoBehaviour
+{
+
+	public FloatData UpSpeed;
+	public FloatData DownSpeed;
+	private bool Upwards;
+	private bool GameOver = false;
+
+	void Start()
+	{
+		Upwards = true;
+		StartCoroutine(Timer());
+	}
+
+	void Update()
+	{
+		if (Upwards)
+		{
+			transform.Translate(Vector3.up*UpSpeed.Value*Time.deltaTime);
+		}
+
+		else {
+			transform.Translate(Vector3.down*DownSpeed.Value*Time.deltaTime);
+		}
+	}
+	IEnumerator Timer()
+    {
+        while (!GameOver)
+        {
+            yield return new WaitForSeconds(2);
+            if (Upwards)
+            {
+                Upwards = false;
+            }
+            else
+            {
+                Upwards = true;
+            }
+        }
+    }
+	
+}
