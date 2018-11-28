ca1c846e4e71f91629e5d8e131b7c18f6c12ff5a
Unknown <skyesladeut@outlook.com>
2018-11-28 16:22:33 -0700

    Worked on Game

---
+++ RunnerGame/Assets/Runner/SCripts/ChangeScene.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+[CreateAssetMenu]
+public class ChangeScene : ScriptableObject
+{
+	public string SceneName;
+
+	public void SceneChange()
+	{
+		SceneManager.LoadScene(SceneName);
+	}
+
+}
---
+++ RunnerGame/Assets/Runner/SCripts/OnEnterKeyEvent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnEnterKeyEvent : MonoBehaviour
+{
+
+	public UnityEvent Event;
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Return))
+		{
+			Event.Invoke();
+		}
+	}
+}
