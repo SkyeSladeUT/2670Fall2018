7984e3057573c3b635841a6e0b689f4036754387
Unknown <skyesladeut@outlook.com>
2018-12-03 13:42:40 -0700

    Game

--- RunnerGame/Assets/Runner/SCripts/CharacterController/Sliding.cs
+++ RunnerGame/Assets/Runner/SCripts/CharacterController/Sliding.cs
@@ -10,23 +10,38 @@ public class Sliding : MonoBehaviour
 	private Vector3 rotation;
 	private Animator anim;
 	public Animator PenguinAnim;
+	private bool CanSlide;
+	public float seconds;
 
 	private void Start()
 	{
 		anim = GetComponent<Animator>();
+		CanSlide = true;
 	}
 
 	private void Update()
 	{
-		if (Input.GetKeyDown(KeyCode.DownArrow))
+		if (CanSlide)
 		{
-			anim.SetTrigger("Slide");
-			PenguinAnim.SetTrigger("Slide");
+			if (Input.GetKeyDown(KeyCode.DownArrow))
+			{
+				CanSlide = false;
+				anim.SetTrigger("Slide");
+				PenguinAnim.SetTrigger("Slide");
+				StartCoroutine(Timer());
+			}
+
+			if (Input.GetKeyDown(KeyCode.UpArrow))
+			{
+				PenguinAnim.SetTrigger("Jump");
+			}
 		}
-		if(Input.GetKeyDown(KeyCode.UpArrow))
-		{
-			PenguinAnim.SetTrigger("Jump");
-		}
+	}
+
+	IEnumerator Timer()
+	{
+		yield return new WaitForSeconds(seconds);
+		CanSlide = true;
 	}
 
 }
